09c6ad103b6ca6afe0b4176dc3fac275976464f2
Muhammad Rifqi Adli Gumay <88142116+asta-kunn@users.noreply.github.com>
2023-12-06 00:09:32 +0700

    fix memory start and stop location

--- Greedy.py
+++ Greedy.py
@@ -2,26 +2,22 @@ __author__ = "Andrea Rubbi"
 import time
 import tracemalloc
 
-
-def set_cover(universe, subsets,costs):
-    cost=0
+def set_cover(universe, subsets, costs):
+    cost = 0
     elements = set(e for s in subsets for e in s)
     if elements != universe:
         return None
     covered = set()
     cover = []
     while covered != elements:
-        subset = max(subsets, key=lambda s: len(s - covered)/costs[subsets.index(s)])
+        subset = max(subsets, key=lambda s: len(s - covered) / costs[subsets.index(s)])
         cover.append(subset)
-        cost+=costs[subsets.index(subset)]
+        cost += costs[subsets.index(subset)]
         covered |= subset
- 
+
     return cover, cost
- 
 
 def main(input_file, z=time.time()):
-    tracemalloc.clear_traces()  # Me-reset pemantauan memori
-    tracemalloc.start()  # Memulai pemantauan memori
     try:
         with open(input_file, 'r') as file:
             content = file.readlines()
@@ -30,33 +26,32 @@ def main(input_file, z=time.time()):
         S = eval(content[1].split('=')[1].strip())
         P = eval(content[2].split('=')[1].strip())
 
-        universe = set(range(1, m+1))
+        universe = set(range(1, m + 1))
         subsets = [set(x) for x in S]
-
+        tracemalloc.clear_traces()  
+        tracemalloc.start()  # Memulai pemantauan memori sebelum eksekusi algoritma utama
         result = set_cover(universe, subsets, P)
-
         if result is not None:
             cover, cost = result
-            print('covering sets= ', cover, '\n',
-                  'cost= ', cost, '$')
+            print('covering sets= ', cover, '\n', 'cost= ', cost, '$')
             print('time:', time.time() - z)
 
             # Memantau penggunaan memori
             current, peak = tracemalloc.get_traced_memory()
-            print(f"Current memory usage: {(current)} bytes")
-            print(f"Peak memory usage: {(peak)} bytes")
+            print(f"Current memory usage: {current} bytes")
+            print(f"Peak memory usage: {peak} bytes")
         else:
             print("No valid cover found for the given subsets.")
-    finally:
-        tracemalloc.stop()  # Menghentikan pemantauan memori
+        tracemalloc.stop()  # Menghentikan pemantauan memori setelah eksekusi algoritma utama
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     input_file1 = "test_data_20.txt"
     main(input_file1)
-    
+
     input_file2 = "test_data_200.txt"
     main(input_file2)
-    
+
     input_file3 = "test_data_2000.txt"
     main(input_file3)
-
